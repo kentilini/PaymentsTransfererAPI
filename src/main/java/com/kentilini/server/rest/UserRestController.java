2dd4fb2c97232168f49795885b1b7fbaabe76904
Yaroslav Astafiev <kentilini@gmail.com>
2017-10-22 11:38:51 +0300

    add javadoc to all business logic operations

--- src/main/java/com/kentilini/server/rest/UserRestController.java
+++ src/main/java/com/kentilini/server/rest/UserRestController.java
@@ -1,6 +1,8 @@
 package com.kentilini.server.rest;
 
 import com.kentilini.server.entity.User;
+import com.kentilini.server.exception.NoResultException;
+import com.kentilini.server.exception.NonUniqueResultException;
 import com.kentilini.server.service.UserService;
 
 import javax.inject.Inject;
@@ -17,18 +19,40 @@ public class UserRestController {
     @Inject
     UserService userService;
 
+
+    /**
+     *
+     * @summary Returns the array of user Ids.
+     * @param maxCount max number of result should be returned
+     * @param pageNumber number of page from which result should be returned
+     * @return List of User Ids
+     */
     @GET
     @Path("getAllIds/{count}/{page}")
     public List<BigInteger> getAllUserIds(@PathParam("count") int maxCount, @PathParam("page") int pageNumber) {
         return userService.getAllUsersIds(maxCount, pageNumber);
     }
 
+    /**
+     *
+     * @summary Finds user account stored at DB by Id
+     * @param id id of user account to search
+     * @return found User value
+     * @throws NonUniqueResultException if search results will be more than 1
+     * @throws NoResultException if user is not stored at DB
+     */
     @GET
     @Path("{id}")
     public User getUserById(@PathParam("id") BigInteger id) {
         return userService.getUserById(id);
     }
 
+    /**
+     *
+     * @summary Create or Updates User Entity value and stores it into DB. This method is transactional.
+     * @param user new value of user that will be updated or added into DB
+     * @return updated\added user value
+     */
     @PUT
     @Path("addOrUpdateUser")
     public User addOrUpdateUser(User user) {
